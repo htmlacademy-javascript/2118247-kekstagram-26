4c17e0e77ca85ba672a60796eb76951e439a4a84
Maria Dashkova <maria.dashkova03@mail.ru>
2022-06-21 20:44:00 +0300

    fixed  module 2 task 1

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function stringLenghtValidation(str, maxLen)
   if(str.length <= maxLen) {
     return(true);
   }
-    return(false);
+  return(false);
 }
 
 stringLenghtValidation('I love js', 20);
