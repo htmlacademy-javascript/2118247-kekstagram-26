266aef3143ee688af5799c87381d29c9a08f91be
Maria Dashkova <maria.dashkova03@mail.ru>
2022-06-21 20:45:30 +0300

    fixed module 2 task 1 code style

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function randomNumber(min, max)
   if(min > max || min < 0) {
     throw Error;
   }
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 randomNumber(1, 15);
