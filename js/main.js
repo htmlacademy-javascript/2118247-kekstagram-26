343c3b02c80034b1cf856751dca30fba436fb4c7
Maria Dashkova <maria.dashkova03@mail.ru>
2022-06-21 20:38:33 +0300

    fixed  module 2 task 1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
 function randomNumber(min, max)
 {
   if(min > max || min < 0) {
-    return('Error');
+    throw Error;
   }
-  else{
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
 }
 
 randomNumber(1, 15);
 
 function stringLenghtValidation(str, maxLen)
 {
-  if(str !== null && str.length <= maxLen) {
+  if(str.length <= maxLen) {
     return(true);
   }
-  else{
     return(false);
-  }
 }
 
 stringLenghtValidation('I love js', 20);
