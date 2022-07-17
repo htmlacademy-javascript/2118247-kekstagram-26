d2b6315b4d0b127bb14ac89c1b3e9753d42ce185
Maria Dashkova <maria.dashkova03@mail.ru>
2022-07-17 22:04:16 +0300

    complete const close = () => {
      fullScreenPicture.classList.add('hidden');
      document.body.classList.remove('modal-open');
    
      closeButton.removeEventListener('click', close);
      window.removeEventListener('keydown', close);
    };
    
    const showComments = (comments) => {
      let allComments = '';
      comments.forEach((comment) => {
        allComments +=
          `<li class="social__comment">
           <img
              class="social__picture"
              src="${comment.avatar}"
              alt="${comment.name}"
              width="35" height="35">
           <p class="social__text">${comment.message}</p>
          </li>`;
      });
    complete module7-task2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-import {renderPhotos} from './photosRender.js';
+import {generatePosts} from './postsGenerator.js';
+import {renderPosts, bindPostClickEvent} from './postsRender.js';
+import {openPostOnFullScreen} from './openPostOnFullScreen.js';
 
-renderPhotos();
+const posts = generatePosts();
+renderPosts(posts);
+
+bindPostClickEvent((postId) => {
+  const selectedPost = posts.find((post) => post.id === +postId);
+  openPostOnFullScreen(selectedPost);
+});
