abf15854c47dd34f62d2154e60ff7553128c3c73
Maria Dashkova <maria.dashkova03@mail.ru>
2022-07-22 01:14:48 +0300

    fixed newPostCreator

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import {generatePosts} from './posts-generation.js';
 import {renderPosts, bindPostClickEvent} from './posts-rendering.js';
 import {postFullScreenRendering} from './post-full-screen-rendering.js';
-import {createNewPost} from './post-creation.js';
+import {createNewPost} from './newPostCreator.js';
 
 const posts = generatePosts();
 renderPosts(posts);
